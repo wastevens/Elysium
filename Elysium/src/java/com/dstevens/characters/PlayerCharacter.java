246978b7a1f4ca2a64aa332a34d483a88dbb62a6
Drew Stevens <wastevens@gmail.com>
2014-04-05 10:14:04 -0400

    Renaming Vampire to PlayerCharacter.

--- Elysium/src/java/com/dstevens/characters/PlayerCharacter.java
+++ Elysium/src/java/com/dstevens/characters/PlayerCharacter.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.dstevens.characters.attributes.Attribute;
 import com.dstevens.characters.skills.CharacterSkill;
 
-public class Vampire {
+public class PlayerCharacter {
 
+    private long id;
+    
     private Attribute physicalTraits;
     private Attribute socialTraits;
     private Attribute mentalTraits;
