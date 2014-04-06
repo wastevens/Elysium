73a9b149442a4c348b573f4d9f4f2bd01d5cf5da
Drew Stevens <wastevens@gmail.com>
2014-04-06 18:17:13 -0400

    Fixed the persistence configuration to notrely on the Single Data Source approach that I'd used while testing.

--- Elysium/src/java/com/dstevens/configuration/PersistanceConfiguration.java
+++ Elysium/src/java/com/dstevens/configuration/PersistanceConfiguration.java
@@ -1,10 +1,11 @@
 package com.dstevens.configuration;
 
-import javax.sql.DataSource;
+import java.util.Properties;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.*;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -23,16 +24,31 @@ public class PersistanceConfiguration {
        factoryBean.setDataSource(dataSource());
        factoryBean.setJpaVendorAdapter(vendorAdapter);
        factoryBean.setPackagesToScan("com.dstevens");
+       factoryBean.setJpaProperties(hibernateProperties());
        return factoryBean;
     }
     
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.show_sql", "false");
+        properties.setProperty("hibernate.format_sql", "false");
+        properties.setProperty("hibernate.use_sql_comments", "false");
+        properties.setProperty("hibernate.auto_close_session", "true");
+        properties.setProperty("hibernate.c3p0.acquire_increment", "5");
+        properties.setProperty("hibernate.c3p0.idle_test_period", "1800");
+        properties.setProperty("hibernate.c3p0.max_size", "600");
+        properties.setProperty("hibernate.c3p0.max_statements", "50");
+        properties.setProperty("hibernate.c3p0.min_size", "5");
+        properties.setProperty("hibernate.c3p0.timeout", "1800");
+        return properties;
+    }
+
     private DataSource dataSource() {
-        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUrl("jdbc:mysql://localhost:3306/elysium");
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUsername("admin");
         dataSource.setPassword("admin");
-        dataSource.setSuppressClose(true);
         return dataSource;
     }
 
