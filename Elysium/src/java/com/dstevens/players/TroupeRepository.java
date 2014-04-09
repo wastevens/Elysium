fa5c21924736baf726abab1c80b2498f2a8a93d2
Drew Stevens <wastevens@gmail.com>
2014-04-09 08:10:54 -0400

    Extract AbstractAuditableRepository, the probable parent of concrete AuditableRepositories.

--- Elysium/src/java/com/dstevens/players/TroupeRepository.java
+++ Elysium/src/java/com/dstevens/players/TroupeRepository.java
@@ -3,36 +3,13 @@ package com.dstevens.players;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.dstevens.persistence.*;
 import com.dstevens.persistence.auditing.*;
 
 @Repository
-public class TroupeRepository implements AuditableRepository<Troupe> {
-
-    private AuditableRepository<Troupe> repository;
+public class TroupeRepository extends AbstractAuditableRepository<Troupe> {
 
     @Autowired
     public TroupeRepository(TroupeDao dao, AuditableRepositoryProvider repositoryProvider) {
-        this.repository = repositoryProvider.repositoryFor(dao);
-    }
-    
-    public Troupe create(Troupe troupe) {
-        return repository.create(troupe);
-    }
-    
-    public Troupe update(Troupe troupe) {
-        return repository.update(troupe);
-    }
-    
-    public void delete(Troupe troupe) {
-        repository.delete(troupe);
-    }
-    
-    public Troupe undelete(Troupe troupe) {
-        return repository.undelete(troupe);
-    }
-    
-    public void hardDelete(Troupe troupe) {
-        repository.hardDelete(troupe);
+        super(repositoryProvider.repositoryFor(dao));
     }
 }
