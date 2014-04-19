30c7c8775c0656d72c4c755ac726c92009e0413f
Drew Stevens <wastevens@gmail.com>
2014-04-19 13:09:20 -0400

    Slight test reorg

--- Elysium/src/test/integration/java/com/dstevens/characters/PlayerCharacterDaoTest.java
+++ Elysium/src/test/integration/java/com/dstevens/characters/PlayerCharacterDaoTest.java
@@ -22,9 +22,9 @@ public class PlayerCharacterDaoTest {
     private PlayerDao playerDao;
     private PlayerFactory playerFactory;
     
-    private Troupe troupesToDelete;
-    private Player playersToDelete;
-    private PlayerCharacter charactersToDelete;
+    private Troupe troupe;
+    private Player player;
+    private PlayerCharacter character;
 
     @Before
     public void setUp() {
@@ -34,33 +34,33 @@ public class PlayerCharacterDaoTest {
         playerFactory    = APP_CONFIG.getBean(PlayerFactory.class);
         characterDao       = APP_CONFIG.getBean(PlayerCharacterDao.class);
         characterFactory    = APP_CONFIG.getBean(PlayerCharacterFactory.class);
+        
+        troupe = troupeDao.save(troupeFactory.createTroupe("troupe name", Setting.ANARCH));
+        player = playerDao.save(playerFactory.createPlayer("player name", "player email").joinTroupe(troupe));
+        character = characterDao.save(characterFactory.createPlayerCharacter(troupe, player, "character name"));
     }
     
     @After
     public void tearDown() {
-        troupeDao.delete(troupesToDelete.getId());
-        playerDao.delete(playersToDelete.getId());
-        characterDao.delete(charactersToDelete.getId());
+        troupeDao.delete(troupe.getId());
+        playerDao.delete(player.getId());
+        characterDao.delete(character.getId());
     }
     
     @Test
     public void testSave() {
-        Troupe troupe = troupeDao.save(troupeFactory.createTroupe("troupe name", Setting.ANARCH));
-        Player player = playerDao.save(playerFactory.createPlayer("player name", "player email").joinTroupe(troupe));
-        PlayerCharacter character = characterDao.save(characterFactory.createPlayerCharacter(troupe, player, "character name"));
-        troupesToDelete = troupe;
-        playersToDelete = player;
-        charactersToDelete = character;
-        
         assertEquals(playerDao.findOne(player.getId()).getCharacters(), set(character));
         assertEquals(troupeDao.findOne(troupe.getId()).getCharacters(), set(character));
         assertEquals(characterDao.findOne(character.getId()).getTroupe(), troupe);
         assertEquals(characterDao.findOne(character.getId()).getPlayer(), player);
-        
+    }
+    
+    @Test
+    public void testSaveWithAttributes() {
         characterDao.save(character.withPhysicalAttribute(character.getPhysicalAttribute().withRating(7).withFocus(PhysicalAttribute.Focus.STAMINA)).
-                                    withMentalAttribute(character.getMentalAttribute().withRating(5).withFocus(MentalAttribute.Focus.INTELLIGENCE).withFocus(MentalAttribute.Focus.PERCEPTION)).
-                                    withSocialAttribute(character.getSocialAttribute().withRating(3).withFocus(SocialAttribute.Focus.CHARISMA).withFocus(SocialAttribute.Focus.APPEARANCE).withFocus(SocialAttribute.Focus.MANIPULATION)));
-        
+                withMentalAttribute(character.getMentalAttribute().withRating(5).withFocus(MentalAttribute.Focus.INTELLIGENCE).withFocus(MentalAttribute.Focus.PERCEPTION)).
+                withSocialAttribute(character.getSocialAttribute().withRating(3).withFocus(SocialAttribute.Focus.CHARISMA).withFocus(SocialAttribute.Focus.APPEARANCE).withFocus(SocialAttribute.Focus.MANIPULATION)));
+
         PlayerCharacter foundCharacter = characterDao.findOne(character.getId());
         assertEquals(7, foundCharacter.getPhysicalAttribute().getRating());
         assertEquals(set(PhysicalAttribute.Focus.STAMINA), foundCharacter.getPhysicalAttribute().getFocuses());
@@ -68,12 +68,15 @@ public class PlayerCharacterDaoTest {
         assertEquals(set(MentalAttribute.Focus.INTELLIGENCE, MentalAttribute.Focus.PERCEPTION), foundCharacter.getMentalAttribute().getFocuses());
         assertEquals(3, foundCharacter.getSocialAttribute().getRating());
         assertEquals(set(SocialAttribute.Focus.CHARISMA, SocialAttribute.Focus.MANIPULATION, SocialAttribute.Focus.APPEARANCE), foundCharacter.getSocialAttribute().getFocuses());
-        
-        characterDao.save(foundCharacter.withSkill(new CharacterSkill(Skill.ATHLETICS, 2)).
-                                         withSkill(new CharacterSkill(Skill.CRAFTS, 3, "Pottery")));
-        
-        PlayerCharacter characterWithSkills = characterDao.findOne(character.getId());
-        assertEquals(set(new CharacterSkill(Skill.ATHLETICS, 2), new CharacterSkill(Skill.CRAFTS, 3, "Pottery")), characterWithSkills.getSkills());
     }
     
+    @Test
+    public void testSaveWithSkills() {
+        characterDao.save(character.withSkill(new CharacterSkill(Skill.ATHLETICS, 2)).
+                withSkill(new CharacterSkill(Skill.CRAFTS, 3, "Pottery")));
+
+        PlayerCharacter characterWithSkills = characterDao.findOne(character.getId());
+        assertEquals(set(new CharacterSkill(Skill.ATHLETICS, 2), new CharacterSkill(Skill.CRAFTS, 3, "Pottery")), 
+                     characterWithSkills.getSkills());
+    }
 }
