7adc70f87c38c3a37cfe19acc0ae30b2c65cf751
Drew Stevens <wastevens@gmail.com>
2014-04-30 15:57:58 -0400

    Poking testing.

---
+++ Elysium/src/java/com/dstevens/characters/changes/SpendXp.java
@@ -0,0 +1,40 @@
+package com.dstevens.characters.changes;
+
+import javax.persistence.*;
+
+import com.dstevens.characters.PlayerCharacter;
+
+@Entity
+@DiscriminatorValue("SpendXP")
+class SpendXp extends SetTrait {
+
+    @Column(name="rating")
+    private int xp;
+
+    //Hibernate only
+    @Deprecated
+    @SuppressWarnings("unused")
+    private SpendXp() {
+        this(null, null, 0);
+    }
+    
+    protected SpendXp(String id, TraitChangeStatus status, int xp) {
+        super(status, null);
+        this.xp = xp;
+    }
+    
+    protected SpendXp(String id, TraitChangeStatus status, int xp, SetTrait associatedTrait) {
+        super(status, associatedTrait);
+        this.xp = xp;
+    }
+    
+    @Override
+    public final PlayerCharacter apply(PlayerCharacter character) {
+        return character.setXp(character.getXp() - xp);
+    }
+
+    @Override
+    public String describe() {
+        String nextTrait = (hasAssociatedTrait() ? String.format (" for %1$s", associatedTrait().describe()) : "");
+        return String.format("(%1$s) Spend %2$s xp%3$s", status(), xp, nextTrait);    }
+}
