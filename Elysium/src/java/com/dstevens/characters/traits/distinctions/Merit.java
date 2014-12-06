08e7e923295364164d44d125782a1636f7b58444
Drew Stevens <wastevens@gmail.com>
2014-12-06 11:33:03 -0500

    New CharacterMerit / Merit approach, which I think is going to work out cleaner and less confusingly.  Less annotation magic bullshit and manual rehydration.

--- Elysium/src/java/com/dstevens/characters/traits/distinctions/Merit.java
+++
@@ -1,9 +0,0 @@
-package com.dstevens.characters.traits.distinctions;
-
-
-public interface Merit<T extends Enum<?>> extends Distinction<T> {
-    
-    default String getType() {
-        return this.getClass().getAnnotation(MeritAnnotation.class).value();
-    }
-}
