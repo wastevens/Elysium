215716cf1bda1420bab24df201486a518749936b
wastevens <wastevens@gmail.com>
2015-05-09 21:32:41 -0400

    Slowly, painfully, migrating to enums using an id instead of ordinal

--- Elysium/src/java/com/dstevens/characters/traits/skills/Skill.java
+++ Elysium/src/java/com/dstevens/characters/traits/skills/Skill.java
@@ -7,46 +7,48 @@ import com.dstevens.characters.traits.TraitQualities;
 
 public enum Skill implements Trait {
     
-    ACADEMICS(DetailLevel.REQUIRES_FOCUS),
-    ANIMAL_KEN,
-    ATHLETICS,
-    AWARENESS,
-    BRAWL,
-    COMPUTER,
-    CRAFTS(DetailLevel.REQUIRES_SPECIALIZATION),
-    DODGE(DetailLevel.REQUIRES_FOCUS),
-    DRIVE,
-    EMPATHY,
-    FIREARMS,
-    INTIMIDATION,
-    INVESTIGATION,
-    LEADERSHIP,
-    LINGUISTICS(DetailLevel.REQUIRES_FOCUS),
-    LORE(DetailLevel.REQUIRES_FOCUS),
-    MEDICINE,
-    MELEE,
-    OCCULT,
-    PERFORMANCE(DetailLevel.REQUIRES_SPECIALIZATION),
-    SCIENCE(DetailLevel.REQUIRES_SPECIALIZATION),
-    SECURITY,
-    STEALTH,
-    STREETWISE,
-    SUBTERFUGE,
-    SURVIVAL;
+    ACADEMICS(0, DetailLevel.REQUIRES_FOCUS),
+    ANIMAL_KEN(1),
+    ATHLETICS(2),
+    AWARENESS(3),
+    BRAWL(4),
+    COMPUTER(5),
+    CRAFTS(6, DetailLevel.REQUIRES_SPECIALIZATION),
+    DODGE(7, DetailLevel.REQUIRES_FOCUS),
+    DRIVE(8),
+    EMPATHY(9),
+    FIREARMS(10),
+    INTIMIDATION(11),
+    INVESTIGATION(12),
+    LEADERSHIP(13),
+    LINGUISTICS(14, DetailLevel.REQUIRES_FOCUS),
+    LORE(15, DetailLevel.REQUIRES_FOCUS),
+    MEDICINE(16),
+    MELEE(17),
+    OCCULT(18),
+    PERFORMANCE(19, DetailLevel.REQUIRES_SPECIALIZATION),
+    SCIENCE(20, DetailLevel.REQUIRES_SPECIALIZATION),
+    SECURITY(21),
+    STEALTH(22),
+    STREETWISE(23),
+    SUBTERFUGE(24),
+    SURVIVAL(25);
     
+    private final int id;
     private final DetailLevel detailLevel;
 
-    private Skill() {
-        this(DetailLevel.NONE);
+    private Skill(int id) {
+        this(id, DetailLevel.NONE);
     }
     
-    private Skill(DetailLevel detailLevel) {
-        this.detailLevel = detailLevel;
+    private Skill(int id, DetailLevel detailLevel) {
+        this.id = id;
+		this.detailLevel = detailLevel;
     }
     
     @Override
     public int id() {
-    	return 0;
+    	return id;
     }
     
     public DetailLevel detailLevel() {
